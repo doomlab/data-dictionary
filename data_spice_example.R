5bfbe32ed343c3f1f0172d2f11d390587d4728d8
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-09-04 14:42:05 -0500

    upload example

---
+++ data_spice_example.R
@@ -0,0 +1,16 @@
+#write out because you need a csv in the folder
+write.csv(mtcars, "mtcars.csv", row.names = F)
+
+#set working directory
+
+
+#load the library
+library(dataspice)
+
+#start with create_spice
+create_spice()
+
+
+
+write_spice() 
+build_site()
