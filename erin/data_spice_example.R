c2dd563716b6054972e5078e62def4ce05eb5bb4
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2018-09-06 00:27:40 -0500

    working example with mtcars

--- erin/data_spice_example.R
+++ erin/data_spice_example.R
@@ -1,25 +1,30 @@
-#write out because you need a csv in the folder
-write.csv(mtcars, "mtcars.csv", row.names = F)
-
 #set working directory
-setwd("~/OneDrive - Missouri State University/RESEARCH/2 projects/data-dictionary/erin")
-###change this to the thing that's fancy
+#save this file in a folder you want to use
+#this code sets up the working directory as the place this file is saved
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 #load the library
 library(dataspice)
 
 #start with create_spice
+#this creates the folders necessary for what you are doing
 create_spice()
 
-#create a list of files to read
-data_files <- list.files(path = getwd(), #where the files are stored 
-                         pattern = ".csv",
-                         full.names = TRUE) #what type of files they are
+#move your files into the data folder that was just created
 
+#create a list of files to read
+#path is where the files are stored, should be data folder
+#pattern is the type of file, csv/sav/etc.
+#full.names allows you to keep the full path you are using
+data_files <- list.files(path = paste(getwd(), "/data", sep = ""),  
+                         pattern = ".csv",
+                         full.names = TRUE) 
 
 # Attributes --------------------------------------------------------------
 
 #prep and fill in attributes from base files
+#data_path is where the files are stored
+#attributes path should be data/metadata/attributes.csv because of create_spice
 prep_attributes(data_path = data_files,
                 attributes_path = paste(getwd(), "/data/metadata/attributes.csv",
                                         sep = ""))
@@ -28,21 +33,47 @@ prep_attributes(data_path = data_files,
 edit_attributes(filepath = "data/metadata/attributes.csv",
                 outdir = paste(getwd(), "/data/metadata/", sep = ""))
 
-
 # Access ------------------------------------------------------------------
 
 #prep the access file
-prep_access(data_path = getwd(), #maybe real?
+#the current version does not work in dataspice
+#use this source file to update it to something that works
+source("prep_access.R")
+prep_access(data_path = paste(getwd(), "/data", sep = ""), 
             access_path = paste(getwd(), "/data/metadata/access.csv",
                                 sep = ""))
 
+#Shiny app to edit the names of the files 
 edit_access(filepath = "data/metadata/access.csv",
             outdir = paste(getwd(), "/data/metadata/", sep = ""))
 
 
+# Bibliography ------------------------------------------------------------
 
-prep_biblio()
-prep_coverage()
+#install maps package for maps
+edit_biblio(filepath = paste(getwd(), "/data/metadata/biblio.csv", sep = ""),
+            outdir = paste(getwd(), "/data/metadata/", sep = ""), 
+            outfilename = "biblio")
 
+
+# Creators ----------------------------------------------------------------
+
+#edit creator information
+edit_creators(filepath = paste(getwd(), "/data/metadata/creators.csv", sep = ""),
+              outdir = paste(getwd(), "/data/metadata/", sep = ""), 
+              outfilename = "creators", 
+              numCreators = 1)
+
+creators_fix = read.csv(paste(getwd(), "/data/metadata/creators.csv", sep = ""))
+colnames(creators_fix)[4] = "affiliation"
+write.csv(creators_fix, 
+          paste(getwd(), "/data/metadata/creators.csv", sep = ""), 
+          row.names = F)
+
+# Finish Spice ------------------------------------------------------------
+
+#this creates overall file
 write_spice() 
+
+#creates website for data
 build_site()
